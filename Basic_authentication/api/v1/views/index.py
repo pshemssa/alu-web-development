fada9d159f55cc2ba3f4b3865b568b709f433cce
pshemssa <122387932+pshemssa@users.noreply.github.com>
2024-09-19 22:54:15 +0000

    auth

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -14,6 +14,8 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
@@ -25,20 +27,8 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - Aborts
-    """
+    
+    @app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
     abort(401)
 
-
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """ GET /api/v1/forbidden
-    Return:
-      - Aborts
-    """
-    abort(403)
